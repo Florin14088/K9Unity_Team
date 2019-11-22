96f2b231b263964bcf310c3c8d95987a62699b80
Florin14088 <58071952+Florin14088@users.noreply.github.com>
2019-11-22 11:50:27 +0000

    Added a useless script that I will remove later
    Rotating a cube in self space

---
+++ K9_Unity_GAME/Assets/_K9Unity_Scripts/F_Rotate_Test_Script.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class F_Rotate_Test_Script : MonoBehaviour
+{
+    public float xA, yA, zA;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        gameObject.transform.Rotate(xA, yA, zA, Space.Self);
+    }
+}
